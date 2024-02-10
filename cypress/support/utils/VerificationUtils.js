2780738c0ee5e0287c7de1095449688da5408008
“Rajat <rajatvermaa95@gmail.com>
2024-02-11 01:19:09 +0530

    #11: Optimizations for Reporting

--- cypress/support/utils/VerificationUtils.js
+++ cypress/support/utils/VerificationUtils.js
@@ -1,17 +1,17 @@
 const VerificationUtils = {
 
     assertResponseStatusCode: (response, expectedStatusCode) => {
-        console.log(`Asserts that Response Status code is '${expectedStatusCode}'.`)
+        cy.addTestContext(`Asserts that Response Status code is '${expectedStatusCode}'.`)
         expect(response.status).to.equal(expectedStatusCode);   
     },
 
     assertResponseBodyKeyPresent: (responseBody, expectedKeyName) => {
-        console.log(`Asserts that Response Body has property: '${expectedKeyName}'.`)
+        cy.addTestContext(`Asserts that Response Body has property: '${expectedKeyName}'.`)
         expect(responseBody).to.have.property(expectedKeyName);
     },
             
     assertResponseBodyKeyValue: (responseBody, expectedKeyName, expectedValue) => {
-        console.log(`Asserts that Response Body has key: '${expectedKeyName}' with value: '${expectedValue}'.`);
+        cy.addTestContext(`Asserts that Response Body has key: '${expectedKeyName}' with value: '${expectedValue}'.`);
         expect(responseBody[expectedKeyName]).to.equal(expectedValue);
     }    
 
