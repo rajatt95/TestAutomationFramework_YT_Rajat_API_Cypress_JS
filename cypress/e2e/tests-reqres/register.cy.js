12bbba30a83bc502d040f10fc93b5f1db5e80128
“Rajat <rajatvermaa95@gmail.com>
2024-02-11 01:55:14 +0530

    #16: Tests for Register endpoint added

---
+++ cypress/e2e/tests-reqres/register.cy.js
@@ -0,0 +1,55 @@
+import EndpointUtils from '../../support/utils/EndpointUtils';
+import RequestBodyUtils from '../../support/utils/RequestBodyUtils';
+import RequestUtils from '../../support/utils/RequestUtils';
+import ResponseUtils from '../../support/utils/ResponseUtils';
+import VerificationUtils from '../../support/utils/VerificationUtils';
+
+/**
+ * Test suite for API endpoints related to user registration.
+ */
+describe('Register', () => {
+
+    // Define endpoints for register
+    const registerEndpoint = EndpointUtils.REGISTER;
+
+    /**
+     * Test case for making a POST request to register successfully.
+     * @tags {regression}
+     */
+    it('POST Request - Register Successful. @regression', () => {
+        
+        // Make a request to the API endpoint
+        RequestUtils.post(registerEndpoint, RequestBodyUtils.REGISTER_SUCCESSFUL).then((response) => {
+
+            // Parse and Log Response Body   
+            const responseBody = ResponseUtils.parseAndLogResponseBody(response);
+
+            // Assertions to validate the response
+            VerificationUtils.assertResponseStatusCode(response, 200)
+            VerificationUtils.assertResponseBodyKeyPresent(responseBody, 'id')
+            VerificationUtils.assertResponseBodyKeyPresent(responseBody, "token")        
+            
+        });
+    });
+
+    /**
+     * Test case for making a POST request to register unsuccessfully.
+     * @tags {regression}
+     */
+    it('POST Request - Register Unsuccessful. @regression', () => {
+        
+        // Make a request to the API endpoint
+        RequestUtils.post(registerEndpoint, RequestBodyUtils.REGISTER_UNSUCCESSFUL).then((response) => {
+
+            // Parse and Log Response Body   
+            const responseBody = ResponseUtils.parseAndLogResponseBody(response);
+
+            // Assertions to validate the response
+            VerificationUtils.assertResponseStatusCode(response, 400)
+            VerificationUtils.assertResponseBodyKeyPresent(responseBody, "error")
+            VerificationUtils.assertResponseBodyKeyValue(responseBody, "error", "Missing password")
+                    
+        });
+    });
+
+})
--- cypress/support/utils/RequestBodyUtils.js
+++ cypress/support/utils/RequestBodyUtils.js
@@ -1,3 +1,6 @@
+import requestBodyRegisterSuccessful from '../../fixtures/register/register-successful.json';
+import requestBodyRegisterUnsuccessful from '../../fixtures/register/register-unsuccessful.json';
+
 import requestBodyUserCreate from '../../fixtures/users/user_create.json';
 import requestBodyUserUpdatePut from '../../fixtures/users/user_update_put.json';
 
@@ -5,6 +8,10 @@ import requestBodyUserUpdatePut from '../../fixtures/users/user_update_put.json'
  * Utility class for managing and providing request body data for various scenarios.
  */
 class RequestBodyUtils {
+
+    // Register
+    REGISTER_SUCCESSFUL = requestBodyRegisterSuccessful;
+    REGISTER_UNSUCCESSFUL = requestBodyRegisterUnsuccessful;
 
     // Users
     USER_CREATE = requestBodyUserCreate;
