96645cae5525cfe75bc51e216cff6be016332fb5
“Rajat <rajatvermaa95@gmail.com>
2024-02-11 00:30:05 +0530

    #3: Cypress Mochawesome Reports added

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,26 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/api-tests-report',
+    charts: true,
+    reportPageTitle: 'API Tests Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,    
+    overwrite: true,
+    autoOpen: false,
+    code: true,
+    timestamp: 'longDate',
+    showPassed: true,
+    saveAllAttempts: false,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
